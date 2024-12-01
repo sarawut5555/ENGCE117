3317c19b4af36c418e812243340c9d13ec9c62ca
sarawut <sarawutkai99@gmail.com>
2024-12-01 23:23:24 +0700

    edit lab2

--- Lab/Lab2/lab2-3.cpp
+++ Lab/Lab2/lab2-3.cpp
@@ -13,10 +13,10 @@ int main() {
     
     for( int i = 0 ; i < num ; i++ ) {
         printf( "str2[%d] = \"%s\"\n", i, out[ i ] ) ;
-    }
+    }//end for
     printf( "count = %d\n", num ) ;
     return 0 ;
-}
+}//end function
 
 void explode( char str1[], char splitter[], char str2[][ 10 ], int *count ) {
     *count = 0 ;
@@ -28,5 +28,5 @@ void explode( char str1[], char splitter[], char str2[][ 10 ], int *count ) {
         strcpy( str2[ *count ], token ) ;  
         ( *count )++ ;  
         token = strtok( NULL, splitter ) ; 
-    }
-}
+    }//end while
+}//end function
