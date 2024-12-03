cce6d37feb20524a4078862d2629a3e243302827
sarawut <sarawutkai99@gmail.com>
2024-12-03 10:15:48 +0700

    add hw05-4

---
+++ Homework/HW05/hw05-4.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2 ;
+    printf("Input 1 : ") ;
+    scanf( "%d",&num1 ) ;
+    printf("Input 2 : ") ;
+    scanf( "%d",&num2 ) ;
+
+    if( num1 == num2 ) {
+        printf( "Number1 equal Number2" ) ;
+    }
+    else if(num1 > num2) {
+        printf( "Number1 greater than Number2" ) ;
+    }
+    else{
+        printf( "Number1 lower than Number2" ) ;
+    }//end if
+    return 0 ;
+}//end function
