950e79795d21959248c1cdca0031cb56df3a61b3
sarawut <sarawutkai99@gmail.com>
2024-12-11 22:24:01 +0700

    add hw09-12

---
+++ Homework/HW09/hw09-12.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void displayMatrix(int matrix[][5], int size);
+
+int main() {
+    int size;
+    printf("Input cube size: ");
+    scanf("%d", &size);
+
+    int matrix[5][5]; 
+    printf("Input element:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix =\n");
+    displayMatrix(matrix, size);
+
+    int choice;
+    printf("Show zero (1=Left, -1=Right): ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < i; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        printf("Left Matrix =\n");
+    } else if (choice == -1) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        printf("Right Matrix =\n");
+    } else {
+        printf("Invalid input.\n");
+    }
+
+    displayMatrix(matrix, size);
+    return 0;
+}
+
+void displayMatrix(int matrix[][5], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
