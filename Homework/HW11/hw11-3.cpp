635a0560de435f9490061a66fce1c539b786961b
sarawut <sarawutkai99@gmail.com>
2024-12-11 23:28:27 +0700

    add hw11-3

---
+++ Homework/HW11/hw11-3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse( char str1[], char str2[] ) ;
+
+int main() {
+    char text[ 50 ] ;
+    char out[ 50 ] ;
+
+    scanf( "%[^\n]" , text ) ;
+    reverse( text, out ) ;
+    return 0 ;
+}//end function
+
+void reverse( char str1[], char str2[] ) {
+    int len = strlen( str1 ) ;
+    for( int i = 0 ; i < len ; i++ ) {
+        str2[ len - 1 - i ] = str1[ i ] ;
+    }//end for
+    str2[ len ] = '\0' ;
+    printf( "%s", str2 ) ;
+}//end function
